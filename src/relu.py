384a3e98f8d0e1a851780b4d6d09059ad9062de1
abdimoallim <moalim.abdi@outlook.com>
2025-07-24 14:54:51 +0300

    chore: cleanup

--- src/relu.py
+++ src/relu.py
@@ -12,14 +12,10 @@ class ReLU:
   def forward(self, x):
     x = x.astype(np.float32)
     output = np.zeros_like(x)
-
     x_buf = cl.Buffer(self.ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=x)
     output_buf = cl.Buffer(self.ctx, cl.mem_flags.WRITE_ONLY, output.nbytes)
-
     kernel = self.program.relu
     kernel(self.queue, (x.size,), None, x_buf, output_buf, np.int32(x.size))
-
     cl.enqueue_copy(self.queue, output, output_buf)
     self.queue.finish()
-
     return output
